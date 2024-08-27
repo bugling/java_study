89eb70f7afbf9aa3d7e5659f1d98dc72260023ed
bugling <baebuseung@gmail.com>
2024-08-27 21:36:16 +0900

    내부 클래스 test 재반영

--- src/self/nested/test/ex1/Library.java
+++ src/self/nested/test/ex1/Library.java
@@ -10,19 +10,32 @@ public class Library {
         bookCount = size;
     }
 
-    public void addBook(String title, String auther) {
+    public void addBook(String title, String author) {
+        // 검증 로직을 다 처리하고
+        if(bookCount >= books.length) {
+            System.out.println("도서관 저장 공간이 부족합니다. ");
+            return;
+        }
+        
+        // 정상 로직을 처리
+        books[bookCount++] = new Book(title, author);
     }
 
     public void showBooks() {
+        System.out.println("=== 책 목록 출력 ===");
+        for(int i=0; i<bookCount; i++) {
+                                // book의 private 속성에 바로 접근 가능함
+            System.out.println("도서 제목: " + books[i].title + ", 저자: " + books[i].author);
+        }
     }
 
     private static class Book {
         private String title;
-        private String auther;
+        private String author;
 
-        public Book(String title, String auther) {
+        public Book(String title, String author) {
             this.title = title;
-            this.auther = auther;
+            this.author = author;
         }
     }
 }
